7a42ecd1f07475545195bbf4e91acab51863c352
Samet <sametklou1907@gmail.com>
2023-08-03 15:25:55 +0300

    Ignore changed, reformatted code

--- src/main/java/com/ipChecker/ipCheckerV1/ipCheck.java
+++ src/main/java/com/ipChecker/ipCheckerV1/ipCheck.java
@@ -4,26 +4,31 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 public class ipCheck {
-    String clientIP;
 
-    public ipCheck(String clientIP) {
-        this.clientIP = clientIP;
-    }
+  String clientIP;
 
-    public String getClientIP() {
-        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        String clientIP = requestAttributes.getRequest().getHeader("X-Forwarded-For");
-        String[] headerNames = {"X-Forwarded-For", "Proxy-Client-IP", "WL-Proxy-Client-IP", "HTTP_CLIENT_IP", "HTTP_X_FORWARDED_FOR"};
-        for (String header : headerNames) {
-            String value = requestAttributes.getRequest().getHeader(header);
-            if (value != null && !value.isEmpty() && !"unknown".equalsIgnoreCase(value)) {
-                clientIP = value;
-                break;
-            }
-        }
-        if (clientIP == null || clientIP.isEmpty() || "unknown".equalsIgnoreCase(clientIP)) {
-            clientIP = requestAttributes.getRequest().getRemoteAddr();
-        }
-        return clientIP;
+  public ipCheck(String clientIP) {
+    this.clientIP = clientIP;
+  }
+
+  public String getClientIP() {
+    ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+    String clientIP = requestAttributes.getRequest().getHeader("X-Forwarded-For");
+    String[] headerNames = {"X-Forwarded-For", "Proxy-Client-IP", "WL-Proxy-Client-IP",
+        "HTTP_CLIENT_IP", "HTTP_X_FORWARDED_FOR"};
+    for (String header : headerNames) {
+      String value = requestAttributes.getRequest().getHeader(header);
+      if (value != null && !value.isEmpty() && !"unknown".equalsIgnoreCase(value)) {
+        clientIP = value;
+        break;
+      }
     }
+    if (clientIP == null || clientIP.isEmpty() || "unknown".equalsIgnoreCase(clientIP)) {
+      clientIP = requestAttributes.getRequest().getRemoteAddr();
+    }
+    if (clientIP == "0:0:0:0:0:0:0:1") {
+      clientIP = "127.0.0.1";
+    }
+    return clientIP;
+  }
 }
